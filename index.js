a2b028129a4578713faad0c730f2b22cde7ad4d3
Raj Aryan <154800358+Rajaryan827@users.noreply.github.com>
2024-12-23 10:30:42 +0530

    unrestricted cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const voiceID = "kgG7dCoKCfLehAPWkJOE";
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: '*',
   credentials: true
 }));
 const port = 3000;
